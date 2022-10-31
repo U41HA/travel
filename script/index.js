6cebc40da750ce17bf6ee6b86930bf22497e0f09
U41HA <lp.lead-box@yandex.ru>
2022-10-31 16:36:16 +0500

    fix burger JS code

--- script/index.js
+++ script/index.js
@@ -1,3 +1,5 @@
+// Burger Menu scripts
+
 const menuButton = document.querySelector('.header-burger-btn');
 const burgerMenuBg = document.querySelector('.burger-menu-bg');
 const burgerMenu = document.querySelector('.burger-menu');
@@ -8,23 +10,27 @@ function toggleMenu() {
     burgerMenuBg.classList.toggle('active');
 }
 
-menuButton.addEventListener('click', toggleMenu);
-
-burgerMenuClose.addEventListener('click', toggleMenu);
-
-burgerMenuBg.addEventListener( 'click', (clickTarget) => {
-	const menuClick = clickTarget.composedPath().includes(burgerMenu);
+function toggleMenuBg(clickTarget) {
+	let menuClick = clickTarget.composedPath().includes(burgerMenu);
  
 	if (!menuClick) {
 		toggleMenu();
 	}
-})
+}
 
-document.addEventListener('keydown', function(e) {
-	if( e.key === 'Escape' && menuButton.classList.contains('active') ){
+function escapeMenu(e) {
+	if( e.key === 'Escape' && menuButton.classList.contains('active') ) {
 		toggleMenu();
 	}
-});
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+burgerMenuClose.addEventListener('click', toggleMenu);
+
+burgerMenuBg.addEventListener( 'click', toggleMenuBg)
+
+document.addEventListener('keydown', escapeMenu);
 
 
 
